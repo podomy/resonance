5d280fe79bea60261030a41d9fcf437407545141
neiltalap <contact@neiltalap.com>
2026-08-29 20:36:22 +0500

    feature(logs): polling and aggregation of logs
    Signed-off-by: neiltalap <contact@neiltalap.com>

--- main.c
+++ main.c
@@ -100,7 +100,7 @@ static bool addrs_up(void) {
     return (true);
 }
 
-static bool spawn_children(pid_t pids[NODES]) {
+static bool spawn_children(pid_t pids[NODES], int* logfds) {
     const char* nss[NODES] = {NULL, "/var/run/netns/"
                                     "net_namespace_nodeb"};
     int i;
@@ -108,7 +108,13 @@ static bool spawn_children(pid_t pids[NODES]) {
     for (i = 0; i < NODES; i++) {
         pid_t pid;
 
+        int pipedes[2];
+        if (pipe(pipedes) < 0) {
+            return (false);
+        }
+
         pid = fork();
+
         if (pid < 0)
             return (false);
         if (pid == 0) {
@@ -121,32 +127,67 @@ static bool spawn_children(pid_t pids[NODES]) {
                     close(nsfd);
                 }
             }
+
+            // Pipe the stdout, stderr to the
+            // write end of the pipe.
+            dup2(pipedes[1], 1);
+            dup2(pipedes[1], 2);
+
+            // Close and remove pipe from the fd table.
+            close(pipedes[0]);
+            close(pipedes[1]);
+
             execlp("/home/neil/Documents/concord/concord",
                    "concord", (char*)NULL);
             _exit(127);
         }
+
         pids[i] = pid;
+        close(pipedes[1]);
+        logfds[i] = pipedes[0];
     }
     return (true);
 }
 
 static void pump_until(TunMap* map, MediumGrid* grid,
                        RadioParams* radio, NodeList* nodes,
-                       int fds[NODES], pid_t pids[NODES]) {
-    struct pollfd p[NODES];
+                       int fds[NODES], pid_t pids[NODES],
+                       int* logfds) {
+    struct pollfd p[2 * NODES];
     int i, status;
     int alive;
 
+    // Adding fds first.
     for (i = 0; i < NODES; i++) {
         p[i].fd = fds[i];
         p[i].events = POLLIN;
     }
+
+    // Adding the logfds next.
+    for (i = NODES; i < 2 * NODES; i++) {
+        p[i].fd = logfds[i - NODES];
+        p[i].events = POLLIN;
+    }
+
     do {
-        if (poll(p, NODES, 100) > 0) {
+        if (poll(p, 2 * NODES, 100) > 0) {
             for (i = 0; i < NODES; i++) {
                 if (p[i].revents & POLLIN)
                     tun_pump_fd(map, fds[i], grid, radio,
                                 nodes);
+            }
+            for (i = 0; i < NODES; i++) {
+                if (!(p[NODES + i].revents & POLLIN))
+                    continue;
+                char buf[1024];
+                ssize_t n;
+                n = read(logfds[i], buf, sizeof(buf) - 1);
+                if (n > 0) {
+                    buf[n] = '\0';
+                    // Here we print with color.
+                    printf("\033[%dm[node %d]\033[0m %s",
+                           36 + i, i, buf);
+                }
             }
         }
         alive = 0;
@@ -161,6 +202,8 @@ int main(void) {
     Context ctx;
     TunMap map = {0};
     int fds[NODES];
+    // fds of the stdin and stdout of children for logs.
+    int logfds[NODES];
     pid_t pids[NODES];
     int i;
 
@@ -176,18 +219,19 @@ int main(void) {
         return (1);
     if (!addrs_up())
         return (1);
-    if (!spawn_children(pids))
+    if (!spawn_children(pids, logfds))
         return (1);
 
     printf("resonance: %zu nodes, %zu tuns ready\n",
            ctx.nodes.len, map.n);
     pump_until(&map, &ctx.grid, &ctx.radio, &ctx.nodes, fds,
-               pids);
+               pids, logfds);
 
     for (i = 0; i < NODES; i++) {
         kill(pids[i], SIGTERM);
         waitpid(pids[i], NULL, 0);
         close(fds[i]);
+        close(logfds[i]);
     }
     context_free(&ctx);
     run("ip netns del net_namespace_nodeb");
